9dce1052634d7246ce87dc7e3b9081410382550e
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-07-15 12:39:34 -0300

    added event in filter table

--- src/view.js
+++ src/view.js
@@ -1,7 +1,11 @@
 import jobComponent from "./components/jobComponent.js";
+import tecnologyComponent from "./components/tecnologyComponent.js";
 
 const main = document.querySelector("main");
 const btnReadMore = document.getElementById("read-more");
+const filters = document.getElementById("filters");
+const filterArea = document.querySelector(".filterArea");
+
 export default class View {
   static page = 1;
   static async getJobs(command) {
@@ -33,4 +37,56 @@ export default class View {
     const jobsTemplate = jobComponent(moreJobs);
     View._insertMoreJobsInMain(jobsTemplate);
   }
+
+  static _getElementByClass(className) {
+    const element = document.querySelectorAll(className);
+    return element;
+  }
+
+  static _getElementById(id) {
+    const element = document.getElementById(id);
+    return element;
+  }
+
+  static addEventFromTecnologys() {
+    const elements = View._getElementByClass(".tecnology");
+
+    elements.forEach((element) =>
+      element.addEventListener("click", (e) => View._eventFromBtnTecnologys(e))
+    );
+  }
+  static _eventFromBtnTecnologys(e) {
+    const tecnologyName = e.currentTarget.textContent;
+
+    const element = View._getElementById(tecnologyName);
+
+    if (element) return;
+    View._addTecnologyFromParameter(tecnologyName);
+    View._addTecnologyFromFilterTable(tecnologyName);
+  }
+  static _addTecnologyFromFilterTable(tecnologyName) {
+    const tecnologyTemplate = tecnologyComponent(tecnologyName);
+    if (filters.lastElementChild === null)
+      filterArea.classList.toggle("active");
+
+    filters.innerHTML += tecnologyTemplate;
+  }
+
+  static _addTecnologyFromParameter(tecnologyName) {
+    const url = new URL(location);
+
+    const params = new URLSearchParams(url.search);
+    console.log(params.toString());
+    params.has("tecnologys")
+      ? params.set(
+          "tecnologys",
+          `${params.getAll("tecnologys")},${tecnologyName}`
+        )
+      : params.set("tecnologys", `${tecnologyName}`);
+
+    window.history.replaceState({}, "", `${location.pathname}?${params}`);
+  }
+
+  static removeParemeters() {}
+  static _verifyParameters() {}
 }

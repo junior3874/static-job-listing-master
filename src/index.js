ce5bd79a65241e4122ed4a34645cdb8eb58367d0
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-07-17 19:13:26 -0300

    generic changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import Service from "./service.js";
 import View from "./view.js";
 import Controller from "./controller.js";
 
-const apiUrl = "./data.json";
-
-const service = new Service({ apiUrl, elementsForPage: 5 });
+const service = new Service({ apiUrl: "./data.json", elementsForPage: 5 });
 const dependencies = {
   view: View,
   service,

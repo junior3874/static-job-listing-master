b118643cacfe5b563e36d42e979ee5c1ba82a3cf
junior3874 <wellingtonjr53@outlook.com.br>
2021-07-18 08:50:35 -0300

    added filter jobs in function getJobs

--- src/service.js
+++ src/service.js
@@ -1,20 +1,51 @@
 export default class Service {
-  constructor({ apiUrl, elementsForPage }) {
-    this.apiUrl = apiUrl;
-    this.elementsForPage = elementsForPage;
-  }
+	constructor({ apiUrl, elementsForPage }) {
+		this.apiUrl = apiUrl;
+		this.elementsForPage = elementsForPage;
+	}
 
-  async getJobs(page = 1) {
-    const jobs = await fetch(this.apiUrl)
-      .then((response) => response.json())
-      .then((result) => result);
+	async getJobs(page = 1) {
+		const jobs = await fetch(this.apiUrl)
+			.then((response) => response.json())
+			.then((result) => result);
 
-    const paginationJobs = jobs.slice(
-      (page - 1) * this.elementsForPage,
-      page * this.elementsForPage
-    );
+		const tratedJobs = this.tratedJobs(jobs);
 
-    return paginationJobs;
-  }
-  searchJobs() {}
+		const paginationJobs = tratedJobs.slice(
+			(page - 1) * this.elementsForPage,
+			page * this.elementsForPage
+		);
+
+		return paginationJobs;
+	}
+	tratedJobs(jobs) {
+		const url = new URL(location);
+
+		const params = new URLSearchParams(url.search);
+		const [queryParams] = params.getAll("tecnologys");
+		const tratedQueryParams = queryParams?.split("&");
+
+		if (tratedQueryParams) {
+			const tratedJobs = jobs.reduce((acumulator, job) => {
+				const { languages, role, tools } = job;
+
+				const tratedTecnologys = new Array(...languages, role, ...tools);
+
+				const verifyParameters = tratedQueryParams.every((tecnologys) =>
+					tratedTecnologys.includes(tecnologys)
+				);
+
+				if (verifyParameters) {
+					acumulator.push(job);
+
+					return acumulator;
+				} else {
+					return acumulator;
+				}
+			}, []);
+			return tratedJobs;
+		}
+
+		return jobs;
+	}
 }
